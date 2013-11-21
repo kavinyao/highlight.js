f798c53b990a0570f3bec8ba6d2da98f8f4f3129
Kavin Yao <kavinyao@gmail.com>
2013-11-20 16:50:26 -0800

    better handling when anchor === focus

--- highlight.js
+++ highlight.js
@@ -1,27 +1,4 @@
 (function(window, undefined) {
-/**
- * Courtesy of MDN: https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Whitespace_in_the_DOM
- * Version of |data| that doesn't include whitespace at the beginning
- * and end and normalizes all whitespace to a single space.  (Normally
- * |data| is a property of text nodes that gives the text of the node.)
- *
- * @param txt  The text node whose data should be returned
- * @return     A string giving the contents of the text node with
- *             whitespace collapsed.
- */
-function normalize_text(txt) {
-    var data = txt.data;
-    // Use ECMA-262 Edition 3 String and RegExp features
-    data = data.replace(/[\t\n\r ]+/g, " ");
-    if (data.charAt(0) === " ") {
-        data = data.substring(1, data.length);
-    }
-    if (data.charAt(data.length - 1) === " ") {
-        data = data.substring(0, data.length - 1);
-    }
-    return data;
-}
-
 function highlight_selection(doc) {
     var sel = doc.getSelection();
     // only support highlight within TextNode
@@ -35,8 +12,8 @@ function highlight_selection(doc) {
     }
 
     var text_node = sel.anchorNode;
-    var full_text = normalize_text(text_node);
-    var selected = sel.toString().trim(); //whitespace at the boundary are not trimmed
+    var full_text = text_node.data;
+    var selected = full_text.slice(sel.anchorOffset, sel.focusOffset);
     var sides = full_text.split(selected);
 
     // replace original with annotated nodes
